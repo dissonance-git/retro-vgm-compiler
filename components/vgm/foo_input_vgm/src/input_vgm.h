78be59d480cddfad3fcf4b55ea58b7dd239140a9
dissonance <ustreamusa@aim.com>
2026-08-12 00:58:48 -0400

    feat(vgm): attach Genesis source state to VGM input

--- components/vgm/foo_input_vgm/src/input_vgm.h
+++ components/vgm/foo_input_vgm/src/input_vgm.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "input_base.h"
+#include "../../enhancement/genesis_state.h"
 
 class input_vgm : public input_base
 {
@@ -8,6 +9,7 @@ private:
 	bool	m_prefer_jpn_tag;
 	constexpr static uint_fast32_t m_gd3_len_max = 1024*1024*100;
 	bool	m_vgz;
+	gameaudio::vgm::genesis_state m_genesis_state;
 
 protected:
 	virtual void register_player();
@@ -24,5 +26,8 @@ public:
 	static bool g_is_our_path(const char *p_path, const char *p_extension);
 
 private:
+#ifdef LIBVGM_GAMEAUDIO_COMMAND_OBSERVER
+	static void command_observer_callback(void* user_param, const VGM_COMMAND_OBSERVER_EVENT* event);
+#endif
 	void guess_track_number_tag_from_file_name(file_info& p_info);
 };
