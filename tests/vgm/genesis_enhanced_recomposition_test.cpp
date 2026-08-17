aae05021ce7b7b27b969078572efd9313736168b
dissonance <ustreamusa@aim.com>
2026-08-17 14:35:10 -0400

    test: follow current spatial path names

--- tests/vgm/genesis_enhanced_recomposition_test.cpp
+++ tests/vgm/genesis_enhanced_recomposition_test.cpp
@@ -122,9 +122,9 @@ int main() {
         assert(std::abs(block.lanes[1].mono_pcm[0] - expected_psg0) < 1.0e-6f);
 
         if (surround)
-            assert(resolve_spatial_playback(options) == spatial_playback_path::source_spatial);
+            assert(resolve_spatial_playback(options) == spatial_playback_path::source_full_sphere);
         else
-            assert(resolve_spatial_playback(options) == spatial_playback_path::stereo_reference);
+            assert(resolve_spatial_playback(options) == spatial_playback_path::reference_stereo);
     };
 
     check_spatial_combination(false, false);
