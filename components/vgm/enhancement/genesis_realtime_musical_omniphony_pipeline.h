58329692280757e124ab06b96f724226a1752f97
dissonance <ustreamusa@aim.com>
2026-08-17 15:54:57 -0400

    vgm: feed timed Genesis evidence into Omniphony

--- components/vgm/enhancement/genesis_realtime_musical_omniphony_pipeline.h
+++ components/vgm/enhancement/genesis_realtime_musical_omniphony_pipeline.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "genesis_spatial_source_bus.h"
+#include "genesis_timed_spatial_source_bus.h"
 #include "../../../model/realtime_musical_omniphony_pipeline.h"
 
 #include <array>
@@ -30,7 +30,7 @@ public:
     using source_array = std::array<genesis_stereo_source_view, source_capacity>;
     using evidence_array =
         std::array<vgmtooling::model::spatial_source_evidence, source_capacity>;
-    using spatial_bus_type = genesis_spatial_source_bus_storage<MaxFrames>;
+    using spatial_bus_type = genesis_timed_spatial_source_bus_storage<MaxFrames, MaxEvents>;
     using pipeline_type = vgmtooling::model::realtime_musical_omniphony_pipeline<
         source_capacity,
         MaxEvents,
@@ -72,8 +72,44 @@ public:
         std::uint32_t ramp_frames,
         float persistent_part_min_confidence = 0.75f) noexcept
     {
+        return process_selected_sources_timed(
+            selected_sources,
+            evidence,
+            nullptr,
+            0,
+            frame_count,
+            sample_rate,
+            interleaved_source_scratch,
+            interleaved_source_capacity,
+            stereo_output,
+            stereo_output_capacity,
+            absolute_sample_position,
+            ramp_frames,
+            persistent_part_min_confidence);
+    }
+
+    genesis_realtime_musical_omniphony_result process_selected_sources_timed(
+        const source_array& selected_sources,
+        const evidence_array& initial_evidence,
+        const vgmtooling::model::spatial_source_evidence_event* evidence_events,
+        std::size_t evidence_event_count,
+        std::size_t frame_count,
+        double sample_rate,
+        float* interleaved_source_scratch,
+        std::size_t interleaved_source_capacity,
+        float* stereo_output,
+        std::size_t stereo_output_capacity,
+        std::uint64_t absolute_sample_position,
+        std::uint32_t ramp_frames,
+        float persistent_part_min_confidence = 0.75f) noexcept
+    {
         genesis_realtime_musical_omniphony_result result{};
-        if (!spatial_bus_.build(selected_sources, evidence, frame_count))
+        if (!spatial_bus_.build(
+                selected_sources,
+                initial_evidence,
+                frame_count,
+                evidence_events,
+                evidence_event_count))
             return result;
 
         result.source_block_valid = true;
